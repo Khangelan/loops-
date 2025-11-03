0a6352f55a3fa55f21caeebe3eb8fbd416a847b2
Khangelani Mhlakane <kmhlakane@gmail.com>
2025-11-03 15:58:27 +0200

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,28 @@
+const numbers = [1, 2, 3, 4, 5]
+
+for (number of numbers) {
+    console.log(number);
+}
+
+
+x = 5
+
+while (x > 0) {
+    console.log(i)
+    x -= 1
+}
+
+
+for (number of numbers) {
+    if (number % 2 === 0) {
+        console.log(number);
+    }    
+}
+
+let sum = 0;
+
+for (number of numbers) {
+    sum += number;
+}
+
+console.log(sum)
